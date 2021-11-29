3026abf14809f2f56b0e21573624ad24561a60ec
awaddell77 <waddell.andrew@gmail.com>
2021-11-28 17:29:50 -0800

    Improvements

--- Parser.py
+++ Parser.py
@@ -3,11 +3,16 @@
 class Parser:
 	def __init__(self,p_str):
 		if not isinstance(p_str,str): raise TypeError("Must be str")
-		self._p_str = p_str
+		#space must be added to a stripped p_str in order to ensure that the last arg or argval is added to the parameter list
+		self._p_str = p_str.strip() + " "
 	def parse(self):
-		p_lst = []
+		#parameter list
+		p_lst = [] 
 		temp_str = ""
 		q_esc = False
+		#a space has to be added to the end of the string
+
+		
 		for i in range(0, len(self._p_str)):
 			if self._p_str[i] == "\"" and not q_esc: q_esc = True
 			elif self._p_str[i] == "\"" and q_esc: q_esc = False
@@ -22,6 +27,9 @@ class Parser:
 				print("Finished {0}".format(temp_str))
 				p_lst.append(temp_str)
 				temp_str = ""
+
+
+
 		return p_lst
 
 
--- parser_test.py
+++ parser_test.py
@@ -1,4 +1,9 @@
 from Parser import Parser
 
-
-m_inst = Parser("exc test_prog.py -f sdfasdfsd -v sdfasfdsfs")
+test_strs = ["exc test_prog.py -f argval1 -v argval2", "exc test_prog.py -f \"argval1.csv\" -v argval2", "exc test_prog.py -f \"arg val1.csv\" -v argval2"]
+res_lst =[]
+for i in range(0,len(test_strs)): 
+	print("Testing: \"{0}\"".format(test_strs[i]))
+	m_inst = Parser(test_strs[i])
+	res_lst = m_inst.parse()
+	print(res_lst)
